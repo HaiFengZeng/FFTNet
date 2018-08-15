524e7bbb0af373aa3c641dd81a026a1898920cd9
HaiFengZeng <haifengzeng459@gmail.com>
2018-08-14 23:56:03 -0700

    update parameters

--- hparams.py
+++ hparams.py
@@ -9,6 +9,7 @@ hparams = tf.contrib.training.HParams(
   num_freq=1025,
   mcep_dim=24,
   mcep_alpha=0.41,
+  hop=160,
   minf0=40,
   maxf0=500,
   sample_rate=16000,
@@ -19,20 +20,29 @@ hparams = tf.contrib.training.HParams(
   min_level_db=-100,
   ref_level_db=20,
   noise_injecting=True,
+  upsample_factor=200,
 
   # Training:
   use_cuda=True,
   use_local_condition=True,
   batch_size=8,
   sample_size=16000,
-  learning_rate=2e-4,
+  learning_rate=5e-4,
   training_steps=200000,
-  checkpoint_interval=5000,
+  checkpoint_interval=2000,
+  epoches=2000,
 
   # Model
   n_stacks=11,
   fft_channels=256,
   quantization_channels=256,
+  out_channels=2,
+  out_type='Gaussian',
+  upsample_network=True,
+  freq_axis_kernel_size=3,
+  upsample_scales=[10,16],# eq hop
+  # Generate
+  generate_path='generate'
 )
 
 
